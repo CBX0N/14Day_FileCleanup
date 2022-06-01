8340d971085434aea70acd64ae6d1cdfe170c33c
CBX0N <88788571+CBX0N@users.noreply.github.com>
2022-06-01 12:12:02 +0100

    Implemented Empty Directory Cleaner

--- 14day_cleanup.ps1
+++ 14day_cleanup.ps1
@@ -10,11 +10,30 @@ $files = Get-childitem -recurse $files | `
 
 $files = $files.VersionInfo.Filename 
 
+write-host "--------- Cleaning up Old Files ---------"
+
 foreach ($file in $files){
     write-host "Purging : " $file
     remove-item -path $file
 }
 
-write-host "----- DONE -----"
+write-host "----------------- DONE ------------------"
+
+Start-sleep -Seconds 1
+
+write-host "----- Cleaning up Empty Directories -----"
+
+$dirs = Get-ChildItem
+$nonEmptyDirs = Get-ChildItem -Recurse | select Directory 
+$nonEmptyDirs = $nonEmptyDirs.directory.name | Get-Unique
+$emptyDirs = $dirs | Where-Object {$_.Name -notin $nonEmptyDirs}
+
+foreach($emptyDir in $emptyDirs){
+
+    write-host "Purging : " $emptyDir
+    remove-item -path $emptyDir
+}
+
+"----------------- DONE ------------------"
 
 set-location C:\GitHub\14Day_FileCleanup 
